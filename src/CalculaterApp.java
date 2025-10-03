fc758c39798a7081abaf7826efe3f5e2e0bbf3f2
Souta Nakauti <bsi25023@uhe.ac.jp>
2025-10-03 12:17:10 +0900

    init

--- src/CalculaterApp.java
+++ src/CalculaterApp.java
@@ -12,16 +12,19 @@ public class CalculaterApp {
 		System.out.println("2.階乗");
 		
 		int number = scanner.nextInt();
+		
 		if(number == 1) {
 			System.out.println("数字をひとつ入力してください");
 			double argument = scanner.nextDouble();
 			calc.squareNumber(argument);
 			System.out.println("プログラムを終了します");
+			
 		}else if(number == 2){
 			System.out.println("正の整数をひとつ入力してください");
 			int argument1 = scanner.nextInt();
 			calc.factorial(argument1);
 			System.out.println("プログラムを終了します");
+			
 		}else {
 			System.out.println("数字が違います。プログラムを終了します");
 		}
